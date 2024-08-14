f305105157d999045b556d3a0073be2d09b7f866
Harpinder Sandhu <harpindersandhu18@Gmail.com>
2024-08-13 18:43:03 -0600

    Routing in Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Cmp1Component } from './components/cmp1/cmp1.component';
+import { Cmp2Component } from './components/cmp2/cmp2.component';
+import { Cmp3Component } from './components/cmp3/cmp3.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'cmp1',
+    component: Cmp1Component,
+  },
+  {
+    path: 'cmp2',
+    component: Cmp2Component,
+  },
+  {
+    path: 'cmp3',
+    component: Cmp3Component,
+  },
+  {
+    path: '**',
+    redirectTo: 'cmp1',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
